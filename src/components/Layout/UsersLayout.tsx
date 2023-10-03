649e8a78f625125d8f13757e01abbead558815ad
Pham Van duy <duypvph24281@fpt.edu.vn>
2023-10-03 21:57:38 +0700

    create_home_page

--- src/components/Layout/UsersLayout.tsx
+++ src/components/Layout/UsersLayout.tsx
@@ -1,17 +1,20 @@
 import { Outlet } from "react-router-dom"
 import React from 'react'
+import Header from "../Header"
+import Footer from "../Footer"
 
 const UsersLayout = () => {
     return (
         <div>
+
             <header>
-                Header
+                <Header/>
             </header>
             <main>
                 <Outlet />
             </main>
             <footer>
-                Footer
+                <Footer/>
             </footer>
 
         </div>
