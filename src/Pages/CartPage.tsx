8f3465f16aa4a703d1c2ee5ccf07bf713081e8e9
Tuan23979 <tuanpnph20839@fpt.edu.vn>
2023-10-04 22:47:33 +0700

    cart_giohang

--- src/Pages/CartPage.tsx
+++ src/Pages/CartPage.tsx
@@ -1,4 +1,8 @@
 const CartPage=()=>{
-    return ;
+    return (
+       <div>
+         <div className ="text-center  font-bold	text-5xl	">Giỏ hàng</div>
+       </div>
+    );
 }
 export default CartPage;
