a410bf1c12b3abe193eb3b7169c35e6ef7f74e27
Tuan23979 <tuanpnph20839@fpt.edu.vn>
2023-10-06 23:23:23 +0700

    cart_giohang

--- src/Pages/CartPage.tsx
+++ src/Pages/CartPage.tsx
@@ -1,8 +1,20 @@
-const CartPage=()=>{
-    return (
-       <div>
-         <div className ="text-center  font-bold	text-5xl	">Giỏ hàng</div>
-       </div>
-    );
-}
+const CartPage = () => {
+  return (
+    <div>
+      <div className="text-center font-bold text-5xl">Giỏ hàng</div>
+      <div>
+        <table className="table-auto w-full "style={{marginTop:"50px"}}>
+          <thead className="pb-10">
+            <tr className="text-left">
+              <th className="font-semibold pb-10" style={{paddingLeft: "50px" }}>Sản phẩm</th>
+              <th className="font-semibold pb-10 text-center">Số lượng</th>
+              <th className="font-semibold pb-10">Đơn hàng</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+    </div>
+  );
+};
+
 export default CartPage;
