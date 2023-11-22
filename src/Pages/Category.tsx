58bdacaf1e1915c511ff38e82979f8f560816066
Pham Van duy <duypvph24281@fpt.edu.vn>
2023-11-22 21:37:38 +0700

    mongodb server

--- src/Pages/Category.tsx
+++ src/Pages/Category.tsx
@@ -21,10 +21,14 @@ const Category = () => {
             <div className='bg-gray-200  '>
                 <h2 className="font-bold text-[27px] text-center pt-8 pb-8">Danh Mục</h2>
                 <div className="max-w-[1440px] mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 p-10 ">
-                    {categories.slice(0,5).map((category) => (
+                    {categories.slice(0, 5).map((category) => (
                         <div className="p-4 transition duration-300 cursor-pointer ease-in-out transform hover:scale-105">
                             <div className="w-32 h-32 mx-auto mb-3 bg-gray-300 rounded-full overflow-hidden">
-                                <img src={category.img} alt="Category 1" className="object-cover w-full h-full" />
+                                {Array.isArray(category.img) && category.img.length > 0 ? (
+                                    <img src={category.img[0].url} alt="Category 1" className="object-cover w-full h-full" />
+                                ) : (
+                                    <div>No Image</div>
+                                )}
                             </div>
                             <h2 className="text-center text-lg font-semibold transition-colors duration-300 ease-in-out hover:text-blue-600">{category.name}</h2>
                         </div>
