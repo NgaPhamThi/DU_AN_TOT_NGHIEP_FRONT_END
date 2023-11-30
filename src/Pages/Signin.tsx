c6de0cdd5892178c4f8cb66c56ce61b4b7ad53bd
Tuan23979 <tuanpnph20839@fpt.edu.vn>
2023-11-30 22:01:11 +0700

    in_up

--- src/Pages/Signin.tsx
+++ src/Pages/Signin.tsx
@@ -1,124 +1,77 @@
 import React, { useState } from 'react';
-import { useForm } from 'react-hook-form';
-import { signin } from '../api/auth';
 
-const DangNhap = () => {
-  // const [email, setEmail] = useState('');
-  // const [password, setPassword] = useState('');
-  // const [error, setError] = useState('');
+const Signin = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  const handleLogin = (e) => {
+    e.preventDefault();
 
-  // const handleEmailChange = (e:any) => {
-  //   setEmail(e.target.value);
-  //   setError('');
-  // };
-
-
-  // const handlePasswordChange = (e:any) => {
-  //   setPassword(e.target.value);
-  //   setError('');
-  // };
-
- 
-  // const authenticateUser = async (email:string, password:string) => {
-  //   try {
-     
-  //     const response = await fetch('http://localhost:8080/api', {
-  //       method: 'POST',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       },
-  //       body: JSON.stringify({ email, password }),
-  //     });
-
-  //     const data = await response.json();
-
-  //     if (response.ok) {
-  //       return { success: true, data };
-  //     } else {
-  //       return { success: false, error: data.message || 'Xác thực thất bại' };
-  //     }
-  //   } catch (error) {
-  //     console.error('Lỗi trong quá trình xác thực:', error);
-  //     throw new Error('Đã xảy ra lỗi trong quá trình xác thực');
-  //   }
-  // };
-
-
-  // const handleSubmit = async (e:any) => {
-  //   e.preventDefault();
-
- 
-  //   if (email === '' || password === '') {
-  //     setError('Vui lòng nhập đầy đủ email và mật khẩu');
-  //   } else {
-  //     try {
-      
-  //       const response = await authenticateUser(email, password);
-
-  //       if (response.success) {
-      
-  //         console.log('Đăng nhập thành công!');
-  //       } else {
-
-  //         setError(response.error);
-  //       }
-  //     } catch (error) {
-  
-  //       setError('Đã xảy ra lỗi trong quá trình xác thực');
-  //     }
-  //   }
-  // };
-  const { register, handleSubmit, formState: { errors } } = useForm<{ email: string, password: string }>();
-  const onHandleSubmit = async (data: { email: string, password: string }) => {
-    try {
-        const response = await signin(data);
-        console.log(response)
-        localStorage.setItem("token", JSON.stringify(response.data.accessToken))
-        
-    } catch (error) {
-        console.log(error);
+    // Kiểm tra điều kiện
+    if (!email || !password) {
+      alert('Vui lòng điền đầy đủ email và mật khẩu');
+      return;
     }
-}
-console.log(errors);
+
+    // Lấy thông tin từ Local Storage
+    const storedEmail = localStorage.getItem('email');
+    const storedPassword = localStorage.getItem('password');
+
+    // Kiểm tra thông tin đăng nhập
+    if (email === storedEmail && password === storedPassword) {
+      setIsLoggedIn(true);
+      alert('Đăng nhập thành công!');
+    } else {
+      setIsLoggedIn(false);
+      alert('Email hoặc mật khẩu không đúng!');
+    }
+  };
+
+  if (isLoggedIn) {
+    // Hiển thị nội dung sau khi đăng nhập thành công
+    return <div>Đăng nhập thành công!</div>;
+  }
+
   return (
     <section>
-      <div className="login-container">
-        <div className="grid__item large--one-half medium--one-half small--one-whole pd-left110 text-left br-right">
-          <div className="width-80">
-            <h1 className="text-2xl font-bold leading-9 text-black">Đăng nhập</h1>
-            <div className="desc_login">
-              Nếu bạn đã có tài khoản, hãy đăng nhập để tích lũy điểm thành viên và nhận được những ưu đãi tốt hơn!
-            </div>
-            <form className="form-vertical" onSubmit={handleSubmit(onHandleSubmit)}>
-              <label htmlFor="Email">Email</label>
-              <input
-                type="email"
-                id="Email"
-                className="input-full"
-                placeholder="Email"
-                {...register("email")}
-              />
+      <div className="grid__item large--one-half medium--one-half small--one-whole pd-left110 text-left br-right">
+        <div className="width-80">
+          <h1 className="text-2xl font-bold leading-9 text-black">Đăng nhập</h1>
+          <div className="desc_login">Nếu bạn đã có tài khoản, hãy đăng nhập để tích lũy điểm thành viên và nhận được những ưu đãi tốt hơn!</div>
+          <form className="form-vertical" onSubmit={handleLogin}>
+            <label htmlFor="Email">Email</label>
+            <input
+              type="email"
+              id="Email"
+              className="input-full"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
 
-              <label htmlFor="Password">Mật khẩu</label>
+            <label htmlFor="Password">Mật khẩu</label>
+            <input
+              type="password"
+              id="Password"
+              className="input-full"
+              placeholder="Mật khẩu"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <a href="">Quên mật khẩu</a>
+            <p>
               <input
-                type="password"
-                id="Password"
-                className="input-full"
-                placeholder="Mật khẩu"
-                {...register("password")}
+                type="submit"
+                className="bg-black text-white w-full py-2 px-4 rounded-none"
+                value="Đăng nhập"
               />
-              <a href="">Quên mật khẩu</a>
-              <p>
-                <input type="submit" className="bg-black text-white w-full py-2 px-4 rounded-none" value="Đăng nhập" />
-              </p>
-              
-            </form>
-          </div>
+            </p>
+          </form>
         </div>
       </div>
     </section>
   );
 };
 
-export default DangNhap;
+export default Signin;
--- src/Pages/Signup.tsx
+++ src/Pages/Signup.tsx
@@ -1,197 +1,125 @@
 import React, { useState } from 'react';
 
 const Signup = () => {
-  const [formData, setFormData] = useState({
-    first_name: '',
-    last_name: '',
-    email: '',
-    phone: '',
-    password: '',
-    subscribe: false,
-    agreeTerms: false,
-  });
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
-  const [errors, setErrors] = useState({});
-
-  const handleChange = (e) => {
-    const { name, value, type, checked } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: type === 'checkbox' ? checked : value,
-    }));
-
-
-    setErrors((prevErrors) => ({
-      ...prevErrors,
-      [name]: undefined,
-    }));
-  };
-
-  const validateForm = () => {
-    const newErrors = {};
-
-
-    if (!formData.first_name.trim()) {
-      newErrors.first_name = 'Họ không được để trống';
-    }
-
-    if (!formData.last_name.trim()) {
-      newErrors.last_name = 'Tên không được để trống';
-    }
-
-    if (!formData.email.trim()) {
-      newErrors.email = 'Email không được để trống';
-    } else if (!/^\S+@\S+\.\S+$/.test(formData.email)) {
-      newErrors.email = 'Email không hợp lệ';
-    }
-
-    if (!formData.phone.trim()) {
-      newErrors.phone = 'Số điện thoại không được để trống';
-    } else if (!/^\d{10}$/.test(formData.phone)) {
-      newErrors.phone = 'Số điện thoại phải có 10 chữ số';
-    }
-
-    if (!formData.password.trim()) {
-      newErrors.password = 'Mật khẩu không được để trống';
-    }
-
-    if (!formData.agreeTerms) {
-      newErrors.agreeTerms = 'Bạn phải đồng ý với các điều khoản';
-    }
-
-    setErrors(newErrors);
-
-    return Object.keys(newErrors).length === 0;
-  };
-
-  const handleSubmit = (e) => {
+  const handleSignup = (e) => {
     e.preventDefault();
 
-    if (validateForm()) {
-     
-      console.log('Đăng ký thành công:', formData);
-    } else {
-      console.log('Dữ liệu không hợp lệ. Vui lòng kiểm tra lại.');
+    // Kiểm tra điều kiện
+    if (!lastName || !email || !phone || !password) {
+      setError('Vui lòng nhập đầy đủ thông tin');
+      return;
     }
+
+    // Kiểm tra định dạng số điện thoại
+    const phoneRegex = /^0\d{9}$/;
+    if (!phone.match(phoneRegex)) {
+      setError('Số điện thoại không hợp lệ.');
+      return;
+    }
+
+    // Kiểm tra định dạng email
+    const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+    if (!email.match(emailRegex)) {
+      setError('Định dạng email không hợp lệ');
+      return;
+    }
+
+    // Lưu dữ liệu đăng ký vào local storage
+    localStorage.setItem('lastName', lastName);
+    localStorage.setItem('email', email);
+    localStorage.setItem('phone', phone);
+    localStorage.setItem('password', password);
+
+    // Reset các trường đăng ký và thông báo lỗi
+    setLastName('');
+    setEmail('');
+    setPhone('');
+    setPassword('');
+    setError('');
+
+    alert('Đăng ký thành công!');
   };
 
   return (
     <section>
-      <div className="login-container">
-        <div className="grid__item large--one-half medium--one-half small--one-whole pd-left110 text-left ">
-          <div className="width-80">
-            <h1 className="text-2xl font-bold leading-9 text-black">Đăng ký</h1>
-            <div className="desc_login">
-              Hãy đăng ký ngay để tích lũy điểm thành viên và nhận được những ưu đãi tốt hơn!
-            </div>
-            <div className="form-vertical">
-              <form acceptCharset="UTF-8" action="/account" id="create_customer" method="post">
-                <input name="form_type" type="hidden" value="create_customer" />
-                <input name="utf8" type="hidden" value="✓" />
+      <div className="grid__item large--one-half medium--one-half small--one-whole pd-left110 text-left ">
+        <div className="width-80">
+          <h1 className="text-2xl font-bold leading-9 text-black">Đăng ký</h1>
+          <div className="desc_login">
+            Hãy đăng ký ngay để tích lũy điểm thành viên và nhận được những ưu đãi tốt hơn!
+          </div>
+          <div className="form-vertical">
+            <form acceptCharset="UTF-8" action="/account" id="create_customer" method="post">
+              <input name="form_type" type="hidden" value="create_customer" />
+              <input name="utf8" type="hidden" value="✓" />
+              <label htmlFor="username">Tên</label>
+              <input
+                type="text"
+                name="last_name"
+                id="username"
+                className="input-full"
+                placeholder="Tên"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+              />
 
-                <div className="form-group">
-                  <label htmlFor="FirstName">Họ</label>
-                  <input
-                    type="text"
-                    name="first_name"
-                    id="FirstName"
-                    className={`input-full ${errors.first_name ? 'error' : ''}`}
-                    placeholder="Họ"
-                    onChange={handleChange}
-                    value={formData.first_name}
-                  />
-                  {errors.first_name && <span className="error-message">{errors.first_name}</span>}
-                </div>
+              <label htmlFor="Email">Email</label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                className="input-full"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
 
-                <div className="form-group">
-                  <label htmlFor="LastName">Tên</label>
-                  <input
-                    type="text"
-                    name="last_name"
-                    id="LastName"
-                    className={`input-full ${errors.last_name ? 'error' : ''}`}
-                    placeholder="Tên"
-                    onChange={handleChange}
-                    value={formData.last_name}
-                  />
-                  {errors.last_name && <span className="error-message">{errors.last_name}</span>}
-                </div>
+              <label htmlFor="Phone">Số điện thoại</label>
+              <input
+                type="text"
+                name="phone"
+                id="phone"
+                className="input-full"placeholder="Số điện thoại"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+              />
 
-                <div className="form-group">
-                  <label htmlFor="Email">Email</label>
-                  <input
-                    type="email"
-                    name="email"
-                    id="Email"
-                    className={`input-full ${errors.email ? 'error' : ''}`}
-                    placeholder="Email"
-                    onChange={handleChange}
-                    value={formData.email}
-                  />
-                  {errors.email && <span className="error-message">{errors.email}</span>}
-                </div>
+              <label htmlFor="CreatePassword">Mật khẩu</label>
+              <input
+                type="password"
+                name="password"
+                id="CreatePassword"
+                className="input-full"
+                placeholder="Mật khẩu"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
 
-                <div className="form-group">
-                  <label htmlFor="Phone">Số điện thoại</label>
-                  <input
-                    type="text"
-                    name="phone"
-                    id="Phone"
-                    className={`input-full ${errors.phone ? 'error' : ''}`}
-                    placeholder="Số điện thoại"
-                    onChange={handleChange}
-                    value={formData.phone}
-                  />
-                  {errors.phone && <span className="error-message">{errors.phone}</span>}
-                </div>
+              <div id="verified_email" className="clearfix large_form">
+                <input type="checkbox" /> Đăng ký nhận bản tin
+              </div>
 
-                <div className="form-group">
-                  <label htmlFor="CreatePassword">Mật khẩu</label>
-                  <input
-                    type="password"
-                    name="password"
-                    id="CreatePassword"
-                    className={`input-full ${errors.password ? 'error' : ''}`}
-                    placeholder="Mật khẩu"
-                    onChange={handleChange}
-                    value={formData.password}
-                  />
-                  {errors.password && <span className="error-message">{errors.password}</span>}
-                </div>
+              <div id="verified_policy" className="clearfix large_form">
+                <input type="checkbox" /> Tôi đồng ý với các <a href="">điều khoản</a> của TND
+              </div>
 
-                <div className="form-group" id="verified_email">
-                  <input
-                    type="checkbox"
-                    name="subscribe"
-                    id="subscribe"
-                    checked={formData.subscribe}
-                    onChange={handleChange}
-                  />
-                  Đăng ký nhận bản tin
-                </div>
+              {error && <p className="error">{error}</p>}
 
-                <div className="form-group" id="verified_policy">
-                  <input
-                    type="checkbox"
-                    name="agreeTerms"
-                    id="agreeTerms"
-                    checked={formData.agreeTerms}
-                    onChange={handleChange}
-                  />
-                  Tôi đồng ý với các <a href="#">điều khoản</a> của TND
-                  {errors.agreeTerms && <span className="error-message">{errors.agreeTerms}</span>}
-                </div>
-
-                <p>
-                  <input
-                    type="submit"
-                    className="bg-black text-white w-full py-2 px-4 rounded-none"
-                    value="Đăng ký"
-                    onClick={handleSubmit}
-                  />
-                </p>
-              </form>
-            </div>
+              <p>
+              <input
+              type="submit"
+              className="bg-black text-white w-full py-2 px-4 rounded-none"
+              value="Đăng ký"
+              onClick={handleSignup}
+              />
+              </p>
+            </form>
           </div>
         </div>
       </div>
