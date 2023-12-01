f7317d7eecc7e943a74575d987ca287f945abee4
Pham Van duy <duypvph24281@fpt.edu.vn>
2023-12-02 00:03:55 +0700

    create_comment_OrderHistory

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,8 @@ import BlogPage from "./Pages/BlogPage";
 import Login from "./Pages/Login";
 import Admin from "./components/Layout/Admin";
 import Dashboard from "./Pages/Admin/dashboard";
-import CommentManagement from "./Pages/Admin/CommentManagement";
-import Message from "./Pages/Admin/Message";
+import CommentManagement from "./Pages/Admin/Comment/CommentManagement";
+import Message from "./Pages/Admin/Comment/Message";
 import ListCategories from "./Pages/Admin/Categories/ListCategories";
 import AddCategories from "./Pages/Admin/Categories/AddCategories";
 import UpdateCategories from "./Pages/Admin/Categories/UpdateCategories";
@@ -24,6 +24,7 @@ import { addproduct } from "./api/product";
 import ProductManager from "./Pages/Admin/Product/ProductManager";
 import AddProduct from "./Pages/Admin/Product/AddProduct";
 import UpdateProduct from "./Pages/Admin/Product/UpdateProduct";
+import OrderHistory from "./Pages/OrderHistory";
 
 function App() {
   const onHandleAdd = async (product: any) => {
@@ -41,9 +42,10 @@ function App() {
         <Route path="/" element={<UsersLayout />}>
           <Route index element={<HomePage />} />
           <Route path="/product" element={<ProductPage />} />
-          <Route path="/product/:id" element={<DetailPage />} />
+          <Route path="/product/:productId" element={<DetailPage />} />
           <Route path="/blog" element={<BlogPage />} />
           <Route path="/pay" element={<PayPage />} />
+          <Route path="/purchase" element={<OrderHistory />} />
           <Route path="/cart" element={<CartPage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signin" element={<Signin />} />
--- src/Pages/Signin.tsx
+++ src/Pages/Signin.tsx
@@ -1,53 +1,63 @@
 import React, { useState } from 'react';
-
+import { useForm } from 'react-hook-form';
+import { signin } from '../api/auth';
 const Signin = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // const [email, setEmail] = useState('');
+  // const [password, setPassword] = useState('');
+  // const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const handleLogin = (e) => {
-    e.preventDefault();
+  // const handleLogin = (e:any) => {
+  //   e.preventDefault();
 
-    // Kiểm tra điều kiện
-    if (!email || !password) {
-      alert('Vui lòng điền đầy đủ email và mật khẩu');
-      return;
+  //   // Kiểm tra điều kiện
+  //   if (!email || !password) {
+  //     alert('Vui lòng điền đầy đủ email và mật khẩu');
+  //     return;
+  //   }
+
+  //   // Lấy thông tin từ Local Storage
+  //   const storedEmail = localStorage.getItem('email');
+  //   const storedPassword = localStorage.getItem('password');
+
+  //   // Kiểm tra thông tin đăng nhập
+  //   if (email === storedEmail && password === storedPassword) {
+  //     setIsLoggedIn(true);
+  //     alert('Đăng nhập thành công!');
+  //   } else {
+  //     setIsLoggedIn(false);
+  //     alert('Email hoặc mật khẩu không đúng!');
+  //   }
+  // };
+
+  // if (isLoggedIn) {
+  //   // Hiển thị nội dung sau khi đăng nhập thành công
+  //   return <div>Đăng nhập thành công!</div>;
+  // }
+  const { register, handleSubmit, formState: { errors } } = useForm<{ email: string, password: string }>();
+  const onHandleSubmit = async (data: { email: string, password: string }) => {
+    try {
+        const response = await signin(data);
+        console.log(response)
+        localStorage.setItem("token", JSON.stringify(response.data.accessToken))
+
+    } catch (error) {
+        console.log(error);
     }
-
-    // Lấy thông tin từ Local Storage
-    const storedEmail = localStorage.getItem('email');
-    const storedPassword = localStorage.getItem('password');
-
-    // Kiểm tra thông tin đăng nhập
-    if (email === storedEmail && password === storedPassword) {
-      setIsLoggedIn(true);
-      alert('Đăng nhập thành công!');
-    } else {
-      setIsLoggedIn(false);
-      alert('Email hoặc mật khẩu không đúng!');
-    }
-  };
-
-  if (isLoggedIn) {
-    // Hiển thị nội dung sau khi đăng nhập thành công
-    return <div>Đăng nhập thành công!</div>;
   }
-
   return (
     <section>
       <div className="grid__item large--one-half medium--one-half small--one-whole pd-left110 text-left br-right">
         <div className="width-80">
           <h1 className="text-2xl font-bold leading-9 text-black">Đăng nhập</h1>
           <div className="desc_login">Nếu bạn đã có tài khoản, hãy đăng nhập để tích lũy điểm thành viên và nhận được những ưu đãi tốt hơn!</div>
-          <form className="form-vertical" onSubmit={handleLogin}>
+          <form className="form-vertical" onSubmit={handleSubmit(onHandleSubmit)}>
             <label htmlFor="Email">Email</label>
             <input
               type="email"
               id="Email"
               className="input-full"
               placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              {...register("email")}
             />
 
             <label htmlFor="Password">Mật khẩu</label>
@@ -56,8 +66,7 @@ const Signin = () => {
               id="Password"
               className="input-full"
               placeholder="Mật khẩu"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              {...register("password")}
             />
             <a href="">Quên mật khẩu</a>
             <p>
