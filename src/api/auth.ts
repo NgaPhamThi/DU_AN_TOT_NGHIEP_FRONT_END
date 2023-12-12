541ed14b0c4bac3001361a0028dd4c366a6e797a
quangpn <quangpn1254@gmail.com>
2023-12-12 16:50:18 +0700

    update_user

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,9 +1,19 @@
+import { IUser } from "../interfaces/cart"
 import instance from "./instance"
 
 export const signin = (data: { email: string, password: string }) => {
     return instance.post('/signin', data)
 }
+export const getUser = () => {
+    return instance.get('/user')
+}
 
-export const signup = (data: { name: string, email: string, password: string, confirmPassword: string }) => {
-    return instance.post('/signup', data)
+export const signup = (data: IUser) => {
+   const uri = "/signup"
+    return instance.post(uri, data)
+}
+
+export const deleteUser =(userId:string | number)=>{
+    const uri = "/user/" +userId
+    return instance.delete(uri)
 }
