889dcb9e50d15befbf22112d3d366a2a20bd5704
NTienDuy <105479766+NTienDuy@users.noreply.github.com>
2023-12-26 22:22:27 +0700

    Fix

--- src/Pages/Admin/Vouchers/ListVouchers.tsx
+++ src/Pages/Admin/Vouchers/ListVouchers.tsx
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react";
 import { IVouchers } from "../../../interfaces/vouchers";
 import { getVoucher, deleteVoucher } from "../../../api/vouchers";
 import { Link } from "react-router-dom";
-import { Table, Button, Modal } from "antd";
+import { Table, Button, Modal, Popconfirm } from "antd";
+import { ToastContainer, toast } from "react-toastify";
 
 const ListVouchers = () => {
   const [vouchers, setVouchers] = useState<IVouchers[]>([]);
@@ -37,9 +38,11 @@ const ListVouchers = () => {
           setVouchers((prevVouchers) =>
             prevVouchers.filter((voucher) => voucher._id !== voucherToDelete)
           );
+          toast.success("Delete Susscessfully !", { autoClose: 2000 });
         })
         .catch((error) => {
           console.error("Error deleting voucher:", error);
+          toast.error("Error Delete Vouchers !");
         });
     }
     setVoucherToDelete(null);
@@ -94,17 +97,53 @@ const ListVouchers = () => {
       title: "Actions",
       key: "actions",
       render: (record: any) => (
-        <div>
-          <Button
-            type="primary"
-            danger
-            onClick={() => handleDeleteClick(record._id)}
+        <div className="inline-flex rounded-lg border  border-gray-100 bg-gray-100 p-1">
+          <Link to={`update/${record._id}`}>
+            <button className="inline-flex items-center gap-2 rounded-md px-4 py-2 text-sm text-gray-500 hover:text-gray-700 focus:relative">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="h-4 w-4"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                />
+              </svg>
+              Edit
+            </button>
+          </Link>
+          <Popconfirm
+            title="Bạn có chắc chắn muốn xóa sản phẩm này?"
+            onConfirm={() => handleDeleteClick(record._id)}
+            okText={
+              <button className="text-red-500 hover:text-black">Xóa</button>
+            }
+            cancelText="Hủy"
+            placement="topRight"
           >
-            Delete
-          </Button>
-          <Button type="primary">
-            <Link to={`/admin/vouchers/update/${record._id}`}>Update</Link>
-          </Button>
+            <button className="inline-flex hover:text-red-500 items-center gap-2 rounded-md bg-white px-4 py-2 text-sm text-blue-500 shadow-sm focus:relative">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="h-4 w-4 "
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                />
+              </svg>
+              Delete
+            </button>
+          </Popconfirm>
         </div>
       ),
     },
@@ -113,10 +152,11 @@ const ListVouchers = () => {
   return (
     <div>
       <div className="p-4">
-        <h1 className="text-3xl font-semibold mb-4">List of Vouchers</h1>
+        <ToastContainer />
+        <h1 className="text-3xl font-semibold mb-4">Quản Lý Vouchers</h1>
         <Link to={"add"}>
           <Button className="bg-blue-500 flex items-center gap-2  hover:bg-white text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline max-w-[190px]">
-            Add Vouchers
+            Thêm Vouchers
           </Button>
         </Link>
 
@@ -127,7 +167,7 @@ const ListVouchers = () => {
           onOk={handleConfirmDelete}
           onCancel={() => setVisible(false)}
         >
-          <p>Are you sure you want to delete this voucher?</p>
+          <p>Xóa?</p>
         </Modal>
       </div>
     </div>
