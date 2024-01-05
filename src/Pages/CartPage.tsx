f02b52280612b619ce9c2cb2c4d6a37bb2c94c88
duypvph24281 <109609909+duypvph24281@users.noreply.github.com>
2024-01-06 01:07:35 +0700

    css_cart

--- src/Pages/CartPage.tsx
+++ src/Pages/CartPage.tsx
@@ -131,82 +131,167 @@ const CartPage = () => {
     return <h2 className=' text-center py-[150px] font-bold text-[30px]'>Không có sản phẩm nào</h2>
   } else {
     return (
-      <div>
+      // <div>
+      //   <div>
+      //     <ul className='flex'></ul>
+      //   </div>
+      //   <section className='flex gap-8 w-10/12 m-auto py-20 '>
+      //     <section className='basis-4/6'>
+      //       {cartItem.map((item) => {
+      //         console.log('item', item)
+      //         return <CartItem key={item._id} {...item} />
+      //       })}
+
+      //       <div className='border-t-2 flex justify-between'>
+      //         <Link to={'/product'}>
+      //           <button className='border-2  font-semibold p-3 px-5 mt-10'>Tiếp tục mua sắm</button>{' '}
+      //         </Link>
+
+      //         <button className='bg-black text-white font-semibold p-3 px-7 mt-10 '>Cập nhật giỏ hàng </button>
+      //       </div>
+      //     </section>
+      //     <section className='basis-2/6 w-full'>
+      //       <p className='font-semibold'>Mã giảm giá</p>
+      //       <div className='w-full'>
+      //         <select
+      //           className='border w-8/12 py-3 px-2 mt-10'
+      //           value={selectedVoucher}
+      //           onChange={(e) => setSelectedVoucher(e.target.value)}
+      //         >
+      //           <option value='' disabled hidden>
+      //             Chọn voucher
+      //           </option>
+      //           {validVouchers.map((voucher) => (
+      //             <option key={voucher._id} value={voucher.Voucher_Code}>
+      //               {`${voucher.Voucher_Code} - Giảm :${voucher.Discount_Type}vnd`}
+      //             </option>
+      //           ))}
+      //         </select>
+      //         <button className='border w-3/12 py-3 px-2 mt-10 bg-black text-white' onClick={handleApplyVoucher}>
+      //           Áp dụng
+      //         </button>
+      //       </div>
+      //       <section className='bg-zinc-100 mt-12'>
+      //         <div className='p-10'>
+      //           {' '}
+      //           <div className=' pt-5 flex'>
+      //             {' '}
+      //             <span className='grow'>Tổng giỏ hàng</span>
+      //             <span className='text-right '>{cartQty}</span>
+      //           </div>
+      //           <div className=' pt-5 flex'>
+      //             {' '}
+      //             <span className='grow'>Tổng tiền </span>
+      //             <span className='text-right '>{totalPrice} vnd</span>
+      //           </div>
+      //           <div className=' pt-5 flex'>
+      //             {' '}
+      //             <span className='grow'>voucher</span>
+      //             <span className='text-right'>giảm giá {discountPercentage} vnd</span>
+      //           </div>
+      //           {/* <div className="pt-5 flex">
+      //             <span className="grow">Tổng giảm giá</span>
+      //             <span className="text-right">
+      //               {(totalPrice * discountPercentage) / 100} vnd
+      //             </span>
+      //           </div> */}
+      //           <div className='pt-5 flex'>
+      //             <span className='grow'>Tổng tiền sau giảm giá</span>
+      //             <span className='text-right'>{totalPrice - discountPercentage} vnd</span>
+      //           </div>
+      //           <Link to={'/pay'}>
+      //             <button className='bg-black text-white font-semibold p-3 mt-10 w-full'>Thanh toán</button>
+      //           </Link>
+      //         </div>
+      //       </section>
+      //     </section>
+      //   </section>
+      // </div>
+      <div className="flex flex-col md:flex-row w-screen h-full px-14 py-7">
+
+   
+    <div className="w-full flex flex-col h-fit gap-4 p-4 ">
+        <p className="text-blue-900 text-xl font-extrabold">My cart</p>
         <div>
-          <ul className='flex'></ul>
-        </div>
-        <section className='flex gap-8 w-10/12 m-auto py-20 '>
-          <section className='basis-4/6'>
-            {cartItem.map((item) => {
+        {cartItem.map((item) => {
               console.log('item', item)
-              return <CartItem key={item._id} {...item} />
-            })}
+            return <CartItem key={item._id} {...item} />
+           })}
+        
+        </div>
 
-            <div className='border-t-2 flex justify-between'>
-              <Link to={'/product'}>
-                <button className='border-2  font-semibold p-3 px-5 mt-10'>Tiếp tục mua sắm</button>{' '}
-              </Link>
+       
+        
+    </div>
 
-              <button className='bg-black text-white font-semibold p-3 px-7 mt-10 '>Cập nhật giỏ hàng </button>
-            </div>
-          </section>
-          <section className='basis-2/6 w-full'>
+    
+    <div className="flex flex-col w-full md:w-2/3 h-fit gap-4 p-4">
+        <p className="text-blue-900 text-xl font-extrabold">Purchase Resume</p>
+        <div>
+        
             <p className='font-semibold'>Mã giảm giá</p>
             <div className='w-full'>
-              <select
-                className='border w-8/12 py-3 px-2 mt-10'
+             <select
+                 className='border w-8/12 py-3 px-2 mt-1'
                 value={selectedVoucher}
-                onChange={(e) => setSelectedVoucher(e.target.value)}
-              >
+                 onChange={(e) => setSelectedVoucher(e.target.value)}
+             >
                 <option value='' disabled hidden>
-                  Chọn voucher
-                </option>
-                {validVouchers.map((voucher) => (
-                  <option key={voucher._id} value={voucher.Voucher_Code}>
-                    {`${voucher.Voucher_Code} - Giảm :${voucher.Discount_Type}vnd`}
+                   Chọn voucher
+               </option>
+               {validVouchers.map((voucher:any) => (
+                 <option key={voucher._id} value={voucher.Voucher_Code}>
+                  {`${voucher.Voucher_Code} - Giảm :${voucher.Discount_Type}vnd`}
                   </option>
                 ))}
               </select>
-              <button className='border w-3/12 py-3 px-2 mt-10 bg-black text-white' onClick={handleApplyVoucher}>
+              <button className='border w-3/12 py-3 px-2 mt-1  bg-black text-white' onClick={handleApplyVoucher}>
                 Áp dụng
               </button>
+        </div>
+        </div>
+        <div className="flex flex-col p-4 gap-4 text-lg font-semibold shadow-md border rounded-sm">
+            <div className="flex flex-row justify-between">
+                <p className="text-gray-600">Tổng ({cartQty} Items)</p>
+                <p className="text-end font-bold">{totalPrice.toLocaleString()}</p>
             </div>
-            <section className='bg-zinc-100 mt-12'>
-              <div className='p-10'>
-                {' '}
-                <div className=' pt-5 flex'>
-                  {' '}
-                  <span className='grow'>Tổng giỏ hàng</span>
-                  <span className='text-right '>{cartQty}</span>
+            <hr className="bg-gray-200 h-0.5"/>
+            <div className="flex flex-row justify-between">
+                <p className="text-gray-600">Freight</p>
+                <div>
+                <p className="text-end font-bold">{discountPercentage}</p>
                 </div>
-                <div className=' pt-5 flex'>
-                  {' '}
-                  <span className='grow'>Tổng tiền </span>
-                  <span className='text-right '>{totalPrice} vnd</span>
+            </div>
+            <hr className="bg-gray-200 h-0.5"/>
+            <div className="flex flex-row justify-between">
+                <p className="text-gray-600">Discount Coupon</p>
+                <p className="text-gray-500 text-base underline">{((totalPrice - discountPercentage)).toLocaleString()}</p>
+            </div>
+            <hr className="bg-gray-200 h-0.5"/>
+            <div className="flex flex-row justify-between">
+                <p className="text-gray-600">Total</p>
+                <div>
+                <p className="text-end font-bold">{(totalPrice - discountPercentage).toLocaleString()}đ</p>
                 </div>
-                <div className=' pt-5 flex'>
-                  {' '}
-                  <span className='grow'>voucher</span>
-                  <span className='text-right'>giảm giá {discountPercentage} vnd</span>
-                </div>
-                {/* <div className="pt-5 flex">
-                  <span className="grow">Tổng giảm giá</span>
-                  <span className="text-right">
-                    {(totalPrice * discountPercentage) / 100} vnd
-                  </span>
-                </div> */}
-                <div className='pt-5 flex'>
-                  <span className='grow'>Tổng tiền sau giảm giá</span>
-                  <span className='text-right'>{totalPrice - discountPercentage} vnd</span>
-                </div>
-                <Link to={'/pay'}>
-                  <button className='bg-black text-white font-semibold p-3 mt-10 w-full'>Thanh toán</button>
+            </div>
+            <div className="flex gap-2">
+              <Link className="transition-colors text-center text-sm bg-blue-600 hover:bg-blue-700 p-2 rounded-sm w-full text-white text-hover shadow-md"  to={'/pay'}>
+              <button >
+                        Mua Hàng  
+              </button>
+              </Link>
+                
+                <Link className="transition-colors text-sm text-center bg-white border border-gray-600 p-2 rounded-sm w-full text-gray-700 text-hover shadow-md" to={`/`}>
+                <button >
+                        Mua Sắm Thêm
+                </button>
                 </Link>
-              </div>
-            </section>
-          </section>
-        </section>
-      </div>
+                
+                
+            </div>
+        </div>
+    </div>
+</div>
     )
   }
 }
